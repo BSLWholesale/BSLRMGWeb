0117653286129480dbdf49258b0cf9ec0ed13ed7
Kanchan <banswaraws@gmail.com>
2026-01-15 14:29:42 +0530

    Modified in clsMasterEntry.cs added the new class i.e. clsDivision - Kanchan

---
+++ Models/clsMasterEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BSLRMGWEB.Models
+{
+    public class clsMasterEntry
+    {
+    }
+
+    public class clsDivision
+    {
+        public Int64 ID { get; set; }
+        public string Division { get; set; }
+        public int CreatedBy { get; set; }
+        public string CreatedOn { get; set; }
+        public int ModifiedBy { get; set; }
+        public string ModifiedOn { get; set; }
+        public string vErrorMsg { get; set; }
+    }
+
+
+
+
+
+}
