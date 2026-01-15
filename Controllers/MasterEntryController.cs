7318153687993ab31eb50d5b398edbc24c2d1f3f
Kanchan <banswaraws@gmail.com>
2026-01-15 14:31:35 +0530

    Added the new controller i.e. MasterEntryController.cs with controller function Fn_Add_New_Division() - Kanchan

---
+++ Controllers/MasterEntryController.cs
@@ -0,0 +1,54 @@
+﻿using BSLRMGWEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Text;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BSLRMGWEB.Controllers
+{
+    public class MasterEntryController : Controller
+    {
+        // GET: MasterEntry
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult DivisionMaster()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public JsonResult Fn_Add_New_Division(clsDivision objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLRMGAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/MasterEntry/Fn_Add_New_Division", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Division insertion failed." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+
+
+    }
+}
