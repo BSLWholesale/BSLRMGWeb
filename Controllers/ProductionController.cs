c60d41254fe8b34f89c09a9e3b2d542dffed6a84
Ankit <banswaraws@gmail.com>
2026-01-20 17:48:45 +0530

    Added update function and selete auto fill for edit style in js and change class and make api calling function

--- Controllers/ProductionController.cs
+++ Controllers/ProductionController.cs
@@ -41,5 +41,29 @@ namespace BSLRMGWEB.Controllers
                 }
             }
         }
+
+        [HttpPost]
+        public JsonResult Fn_Get_Style(clsStyle objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLRMGAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Production/Fn_Get_Style", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Category inserting failed." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
     }
 }
