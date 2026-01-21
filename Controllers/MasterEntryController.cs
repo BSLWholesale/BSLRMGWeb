d9e3b912deb2429f96271c41cabe7e3a399ddf9f
Kanchan <banswaraws@gmail.com>
2026-01-21 18:51:52 +0530

    Added the controller function Fn_Add_New_Line() in MasterentryController.cs - Kanchan

--- Controllers/MasterEntryController.cs
+++ Controllers/MasterEntryController.cs
@@ -379,6 +379,31 @@ namespace BSLRMGWEB.Controllers
         }
 
 
+        [HttpPost]
+        public JsonResult Fn_Add_New_Line(clsLine objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLRMGAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/MasterEntry/Fn_Add_New_Line", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Insert Line details failed." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+
 
     }
 }
