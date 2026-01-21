d77d74e84a82a8e056cd06bc4d26e08073c6f20e
Kanchan <banswaraws@gmail.com>
2026-01-21 13:16:55 +0530

    Added the controller in Fn_Update_SectionDetails_By_SectionID() in MasterEntryController.cs - Kanchan

--- Controllers/MasterEntryController.cs
+++ Controllers/MasterEntryController.cs
@@ -338,5 +338,32 @@ namespace BSLRMGWEB.Controllers
         }
 
 
+
+        [HttpPost]
+        public JsonResult Fn_Update_SectionDetails_By_SectionID(clsSection objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLRMGAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/MasterEntry/Fn_Update_SectionDetails_By_SectionID", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Update Section details failed." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+
+
     }
 }
