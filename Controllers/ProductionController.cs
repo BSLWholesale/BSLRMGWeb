dfd0624545c400a4439c7f49f202fc1923519f91
Ankit <banswaraws@gmail.com>
2026-01-21 15:14:17 +0530

    Added production order page content nad layout and added class and make js function for auto complete

--- Controllers/ProductionController.cs
+++ Controllers/ProductionController.cs
@@ -18,6 +18,11 @@ namespace BSLRMGWEB.Controllers
             return View();
         }
 
+        public ActionResult ProductionOrder()
+        {
+            return View();
+        }
+
         [HttpPost]
         public JsonResult Fn_Insert_Style(clsStyle objReq)
         {
@@ -61,7 +66,39 @@ namespace BSLRMGWEB.Controllers
                 }
                 else
                 {
-                    return Json(new { success = false, message = "Category inserting failed." }, JsonRequestBehavior.AllowGet);
+                    return Json(new { success = false, message = "Style inserting failed." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Fn_AutoComplete_Textbox(string Prefix, string strTBLName, string strFieldName)
+        {
+
+            using (var client = new HttpClient())
+            {
+                var obj = new clsAutoCompliteRequest();
+                obj.SearchKeyword = Prefix;
+                obj.TableName = strTBLName;
+                obj.FieldName = strFieldName;
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLRMGAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Production/Fn_AutoComplete_Textbox", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "No data found." }, JsonRequestBehavior.AllowGet);
+
                 }
             }
         }
--- Models/clsEmployee.cs
+++ Models/clsEmployee.cs
@@ -48,6 +48,48 @@ namespace BSLRMGWEB.Models
         public string vErrorMsg { get; set; }
     }
 
+    public class clsProductionMastr
+    {
+        public Int64 ProductionOrderNo { get; set; }
+        public string OrderDate { get; set; }
+        public string ProductionDeliveryDate { get; set; }
+        public string Merchandiser { get; set; }
+        public int SalesOrderNo { get; set; }
+        public int PONo { get; set; }
+        public int FabIndNo { get; set; }
+        public int OrderQty { get; set; }
+        public string StyleNo { get; set; }
+        public string StyleName { get; set; }
+        public string Buyer { get; set; }
+        public string Brand { get; set; }
+        public string PlantName { get; set; }
+        public int CreatedBy { get; set; }
+        public string CreatedOn { get; set; }
+        public int ModifiedBy { get; set; }
+        public string ModifiedOn { get; set; }
+        public string vErrorMsg { get; set; }
+        public int vErrorCode { get; set; }
+        public string vQueryType { get; set; }
+        public List<clsProductionDetail> _ODetail { get; set; }
+    }
+
+    public class clsProductionDetail
+    {
+        public Int64 ID { get; set; }
+        public Int64 ProductionOrderNo { get; set; }
+        public string ShadeNo { get; set; }
+        public string QualityNo { get; set; }
+        public string Color { get; set; }
+        public string SizeName { get; set; }
+        public int Qty { get; set; }
+        public int CreatedBy { get; set; }
+        public string CreatedOn { get; set; }
+        public int ModifiedBy { get; set; }
+        public string ModifiedOn { get; set; }
+        public string vErrorMsg { get; set; }
+        public int vErrorCode { get; set; }
+        public string vQueryType { get; set; }
+    }
     public class clsRequestDropdown
     {
         public string vFieldName { get; set; }
@@ -63,4 +105,14 @@ namespace BSLRMGWEB.Models
         public string vErrorMsg { get; set; }
     }
 
+    public class clsAutoCompliteRequest
+    {
+        public string SearchKeyword { get; set; }
+        public string TableName { get; set; }
+        public string FieldName { get; set; }
+    }
+    public class clsAutoCompliteResponse
+    {
+        public string SearchKeyword { get; set; }
+    }
 }
