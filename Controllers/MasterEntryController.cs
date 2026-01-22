f9d0a2689c26baa7e697dcf986635ecf92737bc2
Kanchan <banswaraws@gmail.com>
2026-01-22 17:49:23 +0530

    Modified in MasterEntryController.cs added the controller function Fn_Update_LineDetails_By_LineID() - Kanchan

--- Controllers/MasterEntryController.cs
+++ Controllers/MasterEntryController.cs
@@ -428,5 +428,29 @@ namespace BSLRMGWEB.Controllers
         }
 
 
+        public JsonResult Fn_Update_LineDetails_By_LineID(clsLine objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLRMGAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/MasterEntry/Fn_Update_LineDetails_By_LineID", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Updating the Line details failed." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+
     }
 }
