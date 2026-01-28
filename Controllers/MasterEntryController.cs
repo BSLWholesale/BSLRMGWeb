b99c34798c55bef5b1efd7b0c95c0b31a7811358
Kanchan <banswaraws@gmail.com>
2026-01-28 15:08:09 +0530

    Added the new controller function Fn_Delete_CustomerDetails_ById() in MasterEntryController.cs - Kanchan

--- Controllers/MasterEntryController.cs
+++ Controllers/MasterEntryController.cs
@@ -544,6 +544,29 @@ namespace BSLRMGWEB.Controllers
         }
 
 
+        [HttpPost]
+        public JsonResult Fn_Delete_CustomerDetails_ById(clsCustomer objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLRMGAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/MasterEntry/Fn_Delete_CustomerDetails_ById", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Deleting the Customer details failed." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
 
 
     }
