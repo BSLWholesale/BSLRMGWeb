36b48de0fc029612f2f4061009b6bfd902ff4468
Kanchan <banswaraws@gmail.com>
2026-02-09 12:58:18 +0530

    New class added i.e. clsColor in clsMasterEntry.cs - Kanchan

--- Models/clsMasterEntry.cs
+++ Models/clsMasterEntry.cs
@@ -379,4 +379,18 @@ namespace BSLRMGWEB.Models
     }
 
 
+    public class clsColor
+    {
+        public Int64 ColorID { get; set; }
+        public string ColorName { get; set; }
+        public int CreatedBy { get; set; }
+        public string CreatedOn { get; set; }
+        public int ModifiedBy { get; set; }
+        public string ModifiedOn { get; set; }
+        public string vErrorMsg { get; set; }
+        public int vErrorCode { get; set; }
+    }
+
+
+
 }
