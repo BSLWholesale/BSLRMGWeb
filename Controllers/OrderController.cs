0b5f1f5f95eb5a5d385b09628e4d8920f94c1141
Ankit <banswaraws@gmail.com>
2026-02-24 15:05:18 +0530

    Created Fn_Check_Exist_style_In_Master js function and API calling function for operation breakdown time

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -248,5 +248,30 @@ namespace BSLRMGWEB.Controllers
                 }
             }
         }
+
+        [HttpPost]
+        public JsonResult Fn_Check_Exist_style_In_Master(clsOPBreackDownMaster objReq)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConfigurationManager.AppSettings["BSLRMGAPIURL"]);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+
+                HttpResponseMessage responsePost = client.PostAsync("api/Order/Fn_Check_Exist_style_In_Master", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Check existing style getting failed" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
     }
 }
