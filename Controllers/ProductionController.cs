98f360d381858d49b2bf44f6d26c0b09aeae7434
Ankit <banswaraws@gmail.com>
2026-04-07 16:29:45 +0530

    Created Bundle Report page content and created js function for display and filters record

--- Controllers/ProductionController.cs
+++ Controllers/ProductionController.cs
@@ -38,6 +38,11 @@ namespace BSLRMGWEB.Controllers
             return View();
         }
 
+        public ActionResult BundleReport()
+        {
+            return View();
+        }
+
         [HttpPost]
         public JsonResult Fn_Insert_Style(clsStyle objReq)
         {
@@ -619,6 +624,30 @@ namespace BSLRMGWEB.Controllers
                 else
                 {
                     return Json(new { success = false, message = "Updating AppEmpId failed." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Fn_Get_Bundle_Report(clsBundleCompile objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLRMGAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Production/Fn_Get_Bundle_Report", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Shade Fetching failed." }, JsonRequestBehavior.AllowGet);
                 }
             }
         }
