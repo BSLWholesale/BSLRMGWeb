8708a79b5d6f2ff2d41f334ae1fbebbdddecc99e
Kanchan <banswaraws@gmail.com>
2026-06-11 18:24:57 +0530

    Modified in HomeController.cs added the new controller Fn_Employee_BirthdayWishes() in HomeController.cs - Kanchan

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -115,5 +115,31 @@ namespace BSLRMGWEB.Controllers
             }
         }
 
+
+        [HttpPost]
+        public JsonResult Fn_Employee_BirthdayWishes(clsEmployee objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLRMGAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Employee/Fn_Employee_BirthdayWishes", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Employee Birthday Wishes not found." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+
     }
 }
