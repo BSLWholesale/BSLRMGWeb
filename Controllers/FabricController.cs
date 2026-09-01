48a532252c18540f9d1baf0a429833d5ee70d6f4
Ankit <banswaraws@gmail.com>
2026-09-01 10:51:05 +0530

    Created API calling function Fn_Get_Fabric_Defects and js function Fn_Get_Fabric_Defects and added css and popup for display defect for selection

--- Controllers/FabricController.cs
+++ Controllers/FabricController.cs
@@ -325,5 +325,29 @@ namespace BSLRMGWEB.Controllers
                 }
             }
         }
+
+        [HttpPost]
+        public JsonResult Fn_Get_Fabric_Defects(clsQADefects objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLRMGAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Fabric/Fn_Get_Fabric_Defects", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Fabric Defects getting failed." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
     }
 }
