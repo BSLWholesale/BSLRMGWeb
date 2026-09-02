b85369c8ad0c48e0daefc89005ec20e95fe51688
Ankit <banswaraws@gmail.com>
2026-09-02 16:16:03 +0530

    Updated Operation page contetn and layout and Updated Fn_Get_Operation_BreackdownFile js function nd edit and cancel and save button  and make function Fn_Edit_OB_Row,  Fn_Save_OB_Row and Fn_Cancel_OB_Row and API calling function

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -649,5 +649,36 @@ namespace BSLRMGWEB.Controllers
         }
 
         #endregion End Fn_Get_Fabric_ColorLits 22-JUN-2026 
+
+
+        #region Start Fn_Update_OpNo_IN_OBD 02-SEP-2026
+
+        [HttpPost]
+        public JsonResult Fn_Update_OpNo_IN_OBD(clsOPBreackDownDetail objReq)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConfigurationManager.AppSettings["BSLRMGAPIURL"]);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+
+                HttpResponseMessage responsePost = client.PostAsync("api/Order/Fn_Update_OpNo_IN_OBD", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "OpNo updating failed" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        #endregion End Fn_Update_OpNo_IN_OBD 02-SEP-2026
+
     }
 }
